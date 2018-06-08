5fc6be5557f0497fb1881b2c970c22b886a2cc88
Michael Flis <michaelflis07@gmail.com>
2018-06-08 15:17:59 -0700

    adds faker and chance packages.  adds randomized data to db for 20k entries

--- seed/accountOverview-seeder.js
+++ seed/accountOverview-seeder.js
@@ -1,76 +1,60 @@
 const AccountOverview = require('../database-mongodb/models/accountOverview')
 //const db = require('../database-mongodb/index');
 
+const faker = require('faker');
+const Chance = require('chance');
+
+let chance = new Chance();
+
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/test'); // TODO
  
-let overviews =   [
-    new AccountOverview({
-      startDate: new Date(),
-      endDate:  new Date(),
-      account_id: 'goooooooooooooooooooD',
-      title: 'F test F coroporation F', 
-      description: 'company@company.com',
-      npsScoreCurrent:  '55',
-      npsScoreHighest: 
-        {
-          npsScore:  '66',
-          date: new Date (),
-          accountOverview_id: '789-59595'
-        },
-      npsScoreLowest: 
-        {
-          npsScore:  '44',
-          date: new Date(),
-          accountOverview_id: '789-59595'
-        },
-      cases: [],
-      users: [],
-      numberOfCasesResolved:  '3333',
-      numberOfUsers: '8888',
-      numberOfMessages:  '10101010',
-      averageTimeToMessageReponse: '3 minutes',
-      averageTimeToCasesResolution: '1 day'  
-    }),
-    new AccountOverview({
-      startDate: new Date(),
-      endDate:  new Date(),
-      account_id: '8______YYYY_______ASDFASDFAF',
-      title: 'G test G coroporation G', 
-      description: 'company@company.com',
-      npsScoreCurrent:  '55',
-      npsScoreHighest: 
-        {
-          npsScore:  '66',
-          date: new Date (),
-          accountOverview_id: '789-59595'
-        },
-      npsScoreLowest: 
-        {
-          npsScore:  '44',
-          date: new Date(),
-          accountOverview_id: '789-59595'
-        },
-      cases: [],
-      users: [],
-      numberOfCasesResolved:  '3333',
-      numberOfUsers: '8888',
-      numberOfMessages:  '10101010',
-      averageTimeToMessageReponse: '3 minutes',
-      averageTimeToCasesResolution: '1 day'  
-    })
-  ];
+  
+const createRandomEntry = () => {
+  return new AccountOverview({
+    createdAt: new Date(),
+    startDate: faker.date.past(),
+    endDate:  new Date(),
+    account_id: faker.random.number(),
+    title: faker.company.companyName(), 
+    description: faker.internet.email(),
+    npsScoreCurrent:  chance.integer({min: -100, max:100}),
+    npsScoreHighest: 
+      {
+        npsScore:  chance.integer({min: -100, max:100}),
+        date: faker.date.recent(),
+        accountOverview_id: faker.random.number()
+      },
+    npsScoreLowest: 
+      {
+        npsScore:  chance.integer({min: -100, max:100}),
+        date: faker.date.recent(),
+        accountOverview_id: faker.random.number()
+      },
+    cases: [],
+    users: [],
+    numberOfCasesResolved:  chance.integer({min: 0, max:2000}),
+    numberOfUsers: chance.integer({min: 3, max:3000}),
+    numberOfMessages:  chance.integer({min: 0, max:30000}),
+    averageTimeToMessageReponse: chance.minute(),
+    averageTimeToCasesResolution: chance.hour()  
+  })
+}
+
   
 //const accountOverview = new AccountOverview (testOverview);
 
 //db.saveAccountOverview(accountOverview, (err, result) => console.log('saved data ...', accountOverview))
 
 let done = 0;
-let len = overviews.length;
+let len = 20000;
 
 for (var i = 0; i < len; i++) {
-  overviews[i].save( (err, result) => {
+  //overviews[i].save( (err, result) => {
+  let randomEntry = createRandomEntry();
+    
+  randomEntry.save( (err, result) => {
     done++;
     console.log('Added overview for account_id:', result.account_id)
     if ( done === len ) {
