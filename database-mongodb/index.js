283b9bbf1f46758288aa85ce63a7885ad61494db
Michael Flis <michaelflis07@gmail.com>
2018-06-08 15:17:19 -0700

    removes schema for accout overviews (moved to models folder)

--- database-mongodb/index.js
+++ database-mongodb/index.js
@@ -12,37 +12,6 @@ db.once('open', () => console.log('mongoose db connection open..'));
 
 // ---- Account Overviews  -----
 
-// const accountOverviewSchema = new mongoose.Schema(
-//   {
-//     startDate: Date,
-//     endDate:  Date,
-//     account_id: String,
-//     title: String, 
-//     description: String,
-//     npsScoreCurrent:  String,
-//     npsScoreHighest: 
-//       {
-//         npsScore:  String,
-//         date: Date,
-//         accountOverview_id: String
-//       },
-//     npsScoreLowest: 
-//       {
-//         npsScore:  String,
-//         date: Date,
-//         accountOverview_id: String
-//       },
-//     cases: [],
-//     users: [],
-//     numberOfCasesResolved:  String,
-//     numberOfUsers: String,
-//     numberOfMessages:  String,
-//     averageTimeToMessageReponse: String,
-//     averageTimeToCasesResolution: String  
-//   }
-// );
-
-// let AccountOverview = mongoose.model('AccountOverview', accountOverviewSchema);
 
 const saveAccountOverview = (data, callback) => {
   console.log('Saving account overview..');
@@ -70,37 +39,6 @@ const getAllAccountOverviews = (callback) => {
   })
 }
 
-// ------
-
-var testOverview =   {
-    startDate: new Date(),
-    endDate:  new Date(),
-    account_id: '1234',
-    title: 'test coroporation', 
-    description: 'company@company.com',
-    npsScoreCurrent:  '55',
-    npsScoreHighest: 
-      {
-        npsScore:  '66',
-        date: new Date (),
-        accountOverview_id: '789-59595'
-      },
-    npsScoreLowest: 
-      {
-        npsScore:  '44',
-        date: new Date(),
-        accountOverview_id: '789-59595'
-      },
-    cases: [],
-    users: [],
-    numberOfCasesResolved:  '3333',
-    numberOfUsers: '8888',
-    numberOfMessages:  '10101010',
-    averageTimeToMessageReponse: '3 minutes',
-    averageTimeToCasesResolution: '1 day'  
-  };
-  
-//saveAccountOverview(testOverview, (err, result) => console.log('saved data ...'))
 
 
 module.exports = {
