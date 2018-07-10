e959ead68b945fc4f64e00d4f69b505219308508
Michael Flis <michaelflis07@gmail.com>
2018-07-10 13:23:28 -0700

    adds knexfile.js for knex mysql and bookshelf

---
+++ knexfile.js
@@ -0,0 +1,17 @@
+const dbConfig = require('./config/dbConfig');
+//const mysql = require('mysql')
+
+
+module.exports = {
+
+  client: 'mysql',
+  connection: dbConfig,
+  migrations: {
+    directory: './database-mysql/migrations'
+  },
+  seeds: {
+    directory: './database-mysql/seeds/dev'
+  },
+  useNullAsDefault: true
+  
+};
